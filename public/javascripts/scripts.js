4823ecc6d9c87ee3ca88c7985837036a13a97b9b
aryajava <muhammad.aryajava@gmail.com>
2024-10-23 11:42:29 +0700

    Add dataTables

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -6,11 +6,44 @@ const hideAlert = (id) => {
     }
   }, 3000);
 };
-document.addEventListener("DOMContentLoaded", () => {
-  if (document.getElementById("success-alert")) {
+$(document).ready(function () {
+  if ($("#success-alert")) {
     hideAlert("success-alert");
   }
-  if (document.getElementById("error-alert")) {
+  if ($("#error-alert")) {
     hideAlert("error-alert");
   }
+  $("#dataTableUsers").DataTable({
+    responsive: true,
+    searching: true,
+    lengthMenu: [3, 10, 100],
+    language: {
+      lengthMenu: "Show _MENU_entries",
+      paginate: {
+        first: null,
+        previous: "Previous",
+        next: "Next",
+        last: null,
+      }
+    },
+    sortable: true,
+  });
 });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   if (document.getElementById("success-alert")) {
+//     hideAlert("success-alert");
+//   }
+//   if (document.getElementById("error-alert")) {
+//     hideAlert("error-alert");
+//   }
+//   const table = new DataTable("#dataTableUsers", {
+//     searchable: true,
+//     sortable: true,
+//     perPageSelect: true,
+//     perPage: 5,
+//     labels: {
+//       perPage: "{select}",
+//     },
+//   });
+// });
