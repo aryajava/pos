29b7b1dc8d8eec6cf491c857d1a49c4913063d92
aryajava <muhammad.aryajava@gmail.com>
2024-10-24 12:42:57 +0700

    Add user form validation middleware

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import { checkSession } from '../middlewares/checkSession.js';
+import { userFormAddValidation, userFormUpdateValidation } from '../middlewares/userValidation.js';
 import User from '../models/User.js';
 const router = express.Router();
 
 router.get('/', checkSession, async function (req, res, next) {
-
   const usersData = await User.findAll();
-  console.log(usersData);
-
   res.render('users/listUser', {
     user: req.session.user,
     title: `POS - Users`,
@@ -19,24 +17,22 @@ router.get('/', checkSession, async function (req, res, next) {
 
 router.get('/add', checkSession, function (req, res, next) {
   res.render('users/formUser', {
+    error: req.flash("error"),
+    success: req.flash("success"),
     title: `POS - Users`,
     titlePage: `Users`,
     titleForm: `Form Add`,
-    user: req.session.user, // Pastikan user dari session dikirim ke template
-    editUser: null // User yang akan diedit
+    user: req.session.user,
+    userData: null
   });
 });
 
-router.post('/add', checkSession, async function (req, res, next) {
+router.post('/add', checkSession, userFormAddValidation, async function (req, res, next) {
   try {
     const { email, name, password, role } = req.body;
-    if (name.trim() === '' || password.trim() === '') {
-      req.flash('error', 'Please fill all fields');
-      return res.redirect('/users/add');
-    }
     if (await User.findByEmail(email)) {
       req.flash('error', 'Email already registered');
-      return res.redirect('/users/add');
+      return res.status(409).redirect('/users/add');
     }
     await User.save(email, name, password, role);
     res.redirect('/users');
@@ -48,27 +44,33 @@ router.post('/add', checkSession, async function (req, res, next) {
 router.get('/edit/:id', checkSession, async function (req, res, next) {
   const { id } = req.params;
   try {
+    if (!await User.findById(id)) {
+      req.flash('error', 'User not found');
+      return res.status(404).redirect('/users');
+    }
     const userData = await User.findById(id);
     res.render('users/formUser', {
+      error: req.flash("error"),
+      success: req.flash("success"),
       title: `POS - Users`,
       titlePage: `Users`,
       titleForm: `Form Edit`,
-      user: req.session.user, // Pastikan user dari session dikirim ke template
-      editUser: userData // User yang akan diedit
+      user: req.session.user,
+      userData
     });
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/edit/:id', checkSession, async function (req, res, next) {
+router.post('/edit/:id', checkSession, userFormUpdateValidation, async function (req, res, next) {
   const { id } = req.params;
   const { email, name, role } = req.body;
-  if (name.trim() === '') {
-    req.flash('error', 'Please fill all fields');
-    return res.redirect(`/users/edit/${id}`);
-  }
   try {
+    if (await User.findByEmail(email)) {
+      req.flash('error', 'Email already registered');
+      return res.status(409).redirect(`/users/edit/${id}`);
+    }
     await User.update(email, name, role, id);
     res.redirect('/users');
   } catch (error) {
