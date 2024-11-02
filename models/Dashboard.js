9e0f527479b4b8c4d1f37d792da3939e7ea93469
aryajava <muhammad.aryajava@gmail.com>
2024-11-03 00:08:17 +0700

    feat: Add revenue sources and financial summary endpoints, refactor monthly earnings query logic

--- models/Dashboard.js
+++ models/Dashboard.js
@@ -3,29 +3,33 @@ export default class Dashboard {
     this.pool = pool;
   }
 
+  static async getFinancialSummary(pool) {
+    try {
+      let query = `SELECT * FROM financialsummary`;
+      const results = await pool.query(query);
+      return results.rows[0];
+    } catch (error) {
+      error.message = "Error getFinancialSummary: " + error.message;
+      throw error;
+    }
+  }
+
   static async getMonthlyEarning(pool, searchDate = '') {
     try {
       let query = `SELECT * FROM monthlyearnings`;
       const params = [];
       const { startdate, enddate } = searchDate;
-      switch (searchDate) {
-        case startdate && !enddate:
-          query += ` WHERE date >= $1`;
-          params.push(startdate);
-          break;
-        case enddate && !startdate:
-          query += ` WHERE date <= $1`;
-          params.push(enddate);
-          break;
-        case startdate && enddate:
-          query += ` WHERE date BETWEEN $1 AND $2`;
-          params.push(startdate, enddate);
-          break;
-        default:
-          break;
-      };
-      console.log(`query: ${query}, params: ${params}`);
 
+      if (startdate && enddate) {
+        query += ` WHERE date BETWEEN $1 AND $2`;
+        params.push(startdate, enddate);
+      } else if (startdate) {
+        query += ` WHERE date >= $1`;
+        params.push(startdate);
+      } else if (enddate) {
+        query += ` WHERE date <= $1`;
+        params.push(enddate);
+      }
       const results = await pool.query(query, params);
       return results.rows;
     } catch (error) {
@@ -33,26 +37,31 @@ export default class Dashboard {
       throw error;
     }
   }
-  static async getTotalSales(pool, searchDate = '') {
+
+  static async getRevenueSources(pool, searchDate = '') {
     try {
-      let query = `SELECT * FROM sales`;
+      let query = ` SELECT 
+        COUNT(CASE WHEN customer = 1 THEN 1 END) as direct, 
+        COUNT(CASE WHEN customer != 1 THEN 1 END) as customer
+        FROM sales `;
       const { startdate, enddate } = searchDate;
       const params = [];
       if (startdate || enddate) {
         if (startdate && !enddate) {
-          query += ` WHERE time >= $1`;
+          query += ` WHERE time::DATE >= $1`;
           params.push(startdate);
         } else if (enddate && !startdate) {
-          query += ` WHERE time <= $1`;
+          query += ` WHERE time::DATE <= $1`;
           params.push(enddate);
         } else {
-          query += ` WHERE time BETWEEN $1 AND $2`;
+          query += ` WHERE time::DATE BETWEEN $1 AND $2`;
           params.push(startdate, enddate);
         }
       }
-      console.log(`query: ${query}, params: ${params}`);
+      const results = await pool.query(query, params);
+      return results.rows[0];
     } catch (error) {
-      error.message = "Error getTotalSales: " + error.message;
+      error.message = "Error getRevenueSources: " + error.message;
       throw error;
     }
   };
--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,55 @@ import moment from 'moment/moment.js';
 const router = express.Router();
 
 export default (pool) => {
-  router.get('/', checkSession, function (req, res, next) {
+  router.get('/', checkSession, async function (req, res, next) {
+    const data = await Dashboard.getMonthlyEarning(pool, {});
+    console.log(`data: ${JSON.stringify(data)}`);
     res.render('index', {
       user: req.session.user,
       title: `Dashboard`,
+      titlePage: `Dashboard`,
+      titleForm: `Date Settings`,
     });
   });
 
   // list monthly earnings
   router.get('/api/monthlyearnings', checkSession, async function (req, res, next) {
     try {
-      const { startdate, enddate } = req.query;
+      let { startdate, enddate } = req.query;
       const data = await Dashboard.getMonthlyEarning(pool, { startdate, enddate });
+
+      console.log(`startdate: ${startdate}, enddate: ${enddate}`);
+      console.log(`data: ${JSON.stringify(data)}`);
+
       data.forEach((item) => {
         item.date = moment(item.date).format('MMM YY');
       });
       res.status(200).json({ data });
     } catch (error) {
       error.message = "Error getMonthlyEaring: " + error.message;
+      next(error);
+    };
+  });
+
+  // revenue sources
+  router.get('/api/revenuesources', async function (req, res, next) {
+    try {
+      const { startdate, enddate } = req.query;
+      const data = await Dashboard.getRevenueSources(pool, { startdate, enddate });
+      res.status(200).json({ data });
+    } catch (error) {
+      error.message = "Error getRevenueSources: " + error.message;
+      next(error);
+    };
+  });
+
+  // financial summary
+  router.get('/api/summary', async function (req, res, next) {
+    try {
+      const data = await Dashboard.getFinancialSummary(pool);
+      res.status(200).json({ data });
+    } catch (error) {
+      error.message = "Error getFinancialSummary: " + error.message;
       next(error);
     };
   });
